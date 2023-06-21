4ea688409f99f652e18946f4b0ffc3765e4da704
João Francisco <116165087+joaocafrancisco@users.noreply.github.com>
2023-06-21 09:14:42 +0100

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header(){
     return(
         <header className={styles.header}>
             <h1>
-                <Link to="/">Game Of Thrones App</Link> 
+                <Link to="/">Game Of Thrones</Link> 
             </h1>
         </header>
     )
